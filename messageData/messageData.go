70ac6316c4dea3b3033108c901b58af00e2a555b
Ken Ramos <vonnduven@gmail.com>
2024-05-21 07:13:46 -0700

    Change json prop format for MessageData

--- messageData/messageData.go
+++ messageData/messageData.go
@@ -1,9 +1,9 @@
 package messageData
 
 type MessageData struct {
-	ChannelID                string `json:"channelId"`
-	MessageContent           string `json:"messageContent"`
-	ReferencedMessageID      string `json:"referencedMessageId"`
-	ReferencedMessageContent string `json:"referencedMessageContent"`
-	ReferencedMessageAuthor  string `json:"referencedMessageAuthor"`
+	ChannelID                string `json:"channel_id"`
+	MessageContent           string `json:"message_content"`
+	ReferencedMessageID      string `json:"referenced_message_id"`
+	ReferencedMessageContent string `json:"referenced_message_content"`
+	ReferencedMessageAuthor  string `json:"referenced_message_author"`
 }
